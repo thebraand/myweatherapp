cb51d8ddf9e888022fc16833855ba937adc61796
thebraand <therese.andresen.92@gmail.com>
2022-01-16 14:45:18 +0100

    Forecast-work
    Working on implementing the forecast

--- src/app.js
+++ src/app.js
@@ -30,6 +30,34 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-3">
+      <div class="weather-forecast-date">${day}</div>
+
+      <img
+        src="http://openweathermap.org/img/wn/04n@2x.png"
+        alt="cloudy"
+        width="36"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18°/</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -38,6 +66,8 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  displayForecast();
 
   celsiusTemperature = response.data.main.temp;
 
