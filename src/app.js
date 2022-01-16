3dbdd5edf45557b01f1e0c0a68e731a31b4fd325
thebraand <therese.andresen.92@gmail.com>
2022-01-16 16:42:48 +0100

    Update app.js
    Working on with the API-integration for forecast

--- src/app.js
+++ src/app.js
@@ -29,7 +29,13 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "8ceca234f09bef7110c2d200b10b2503";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
